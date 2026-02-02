e253ad3ce488ffd71685b4d25b983dd7bfc9f2d9
Bounty2049 <icorp2049@gmail.com>
2026-02-02 21:24:20 +0400

    remove make_db.py file, transferred everything to main.py

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 import sqlite3
-from typing import Optional, List, Tuple, Any
-import datetime
 
-class Database:
-    def __init__(self, db_name: str = "terminij.db"):
-        self.db_name = db_name
-        self.init_db()
 
-        def get_connetion(self):
-            return sqlite3.connect(self.db_name)
-        
-        def init_db(self):
+f = open("Bedingungen.txt", "r", encoding="utf-8")
+
+lines = f.readlines()
+
+termin = []
+description = []
+c = 0
+
+for line in lines:
+    for term in line.split(" - "):
+        if c % 2 == 0: 
+            termin.append(term.strip())
+        else:
+            description.append(term.strip())
+        c += 1; 
+
+
+# Create database and table 
+conn = sqlite3.connect("terms.db")
+
+cursor = conn.cursor()
+
+cursor.execute("CREATE TABLE IF NOT EXISTS bedingungen (id INTEGER PRIMARY KEY AUTOINCREMENT, term TEXT, description TEXT)")
+
+for i in range(len(termin)):
+    cursor.execute("INSERT INTO bedingungen (term, description) VALUES (?, ?)", (termin[i], description[i]))
+
+cursor.execute("SELECT * FROM bedingungen")
+tables = cursor.fetchall()
+for table in tables:
+    print(f"ID: {table[0]}, Term: {table[1]}, Description: {table[2]}")
+    print()
+
+conn.close()
+print("Database and table created, data inserted successfully.")
